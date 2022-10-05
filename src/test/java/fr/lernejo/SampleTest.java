73cc804f72072c95f64f99c2407b689a877cfa9b
Cheikh LO <chheikhlo@gmail.com>
2022-10-06 01:24:28 +0200

    Setup GitHub CI

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,34 @@
+package fr.lernejo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+    @Test
+    void testFactInit(){
+        var Sample = new Sample();
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Sample.fact(-3));
+
+    }
+
+    @Test
+    void testFact(){
+        var Sample = new Sample();
+        int i = Sample.fact(3);
+        Assertions.assertThat(i).isEqualTo(6);
+
+    }
+
+    @Test
+    void testOperation(){
+        var Sample = new Sample();
+        int mult = Sample.op(fr.lernejo.Sample.Operation.MULT,5,7);
+        int add = Sample.op(fr.lernejo.Sample.Operation.ADD,5,7);
+
+        Assertions.assertThat(mult).isEqualTo(35);
+        Assertions.assertThat(add).isEqualTo(12);
+
+    }
+
+}
